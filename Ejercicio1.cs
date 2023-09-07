b09bd9733e03734228e352440a57bba8d7f0e885
EverHb <everbenavides1234@gmail.com>
2023-09-07 14:30:31 -0600

    guia semana 6 Programacion II

---
+++ Ejercicio1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+class whereSample2
+{
+    static void Main() 
+    {
+        int[] scores = { 97, 92, 81, 60 };
+
+        var queryLowNums = from num in scores where num > 80 select num;
+
+        foreach (var s in queryLowNums)
+
+        {
+            Console.WriteLine(s.ToString() + " ");
+        }
+
+        Console.WriteLine();
+
+    }
+
+
+
+}
---
+++ Ejercicio2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+class whereSample3 
+{
+    static void Main()
+    {
+        int[] scores = { 90, 71, 82, 93,75,82 };
+
+        var queryLowNums = from num in scores where IsEven(num) select num;
+
+        foreach (var s in queryLowNums)
+
+        {
+            Console.WriteLine(s.ToString() + " ");
+        }
+
+        
+            static bool IsEven(int i)
+            {
+                return i % 2 == 0;
+            }
+
+        }
+
+    }
+
+
+
